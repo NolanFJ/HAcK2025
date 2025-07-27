45830c3cbac27075118ae8f72ba75b8ea28640c5
dvaldez4 <valdezd@g.ucla.edu>
2025-07-26 19:21:30 -0700

    OLED display

--- pico/main.py
+++ pico/main.py
@@ -4,10 +4,15 @@ from Sensors.Light import Light
 from Sensors.temp import TempHumid
 from Sensors.ultsens import Ultrasonic
 import random
+from machine import Pin, I2C
+import ssd1306
 
 lightSensor = Light(28)
 tempSensor = TempHumid(4)
 ultraSensor = Ultrasonic(3, 2)
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # mainly just testing connection
 def cb(topic, msg):
@@ -17,12 +22,29 @@ def cb(topic, msg):
 # receive a message and display to OLED
 def message(topic, msg):
     if topic == b"display":
+        # Clear the display by filling it with white and then showing the update
+        oled.fill(1)
+        oled.show()
+        sleep(1)  # Wait for 1 second
+
+        # Clear the display again by filling it with black
+        oled.fill(0)
+        oled.show()
+        sleep(1)  # Wait for another second
+
+        text = msg.decode()  # Convert bytes to string
+        oled.text(text, 0, 0)  # Display decoded message at position (0, 0)
+        #oled.text('different hello', 0, 16)  # Display "sunfounder.com" at position (0, 16)
+
+        # The following line sends what to show to the display
+        oled.show()
         print(msg.decode())
+
 
 def main():
     try:
-        connect_internet("", password="")
-        client = connect_mqtt("", "", "")
+        connect_internet("HAcK-Project-WiFi-2", password="UCLA.HAcK.2024.Summer")
+        client = connect_mqtt("07e486cd3c324be6a09b98399a0b48e3.s1.eu.hivemq.cloud", "David", "DavidValdez1")
         
         client.set_callback(cb)
         client.subscribe(b"take_picture")
@@ -67,4 +89,6 @@ def main():
         
 if __name__ == "__main__":
     main()
+
+
 
